47845a765006ea98300f7ca5d082503976504d9f
Jeff Batt <jeffbatt@gmail.com>
2014-03-25 11:11:29 -0600

    Update labels in the json file.

--- menuContent.js
+++ menuContent.js
@@ -6,7 +6,8 @@ var JSONObject=
 {
 	//This is where the page title names will be. I will be updating this to also link to pages. The idea is to dynmaically create the menu based on what is in this JS file.
 	"menu1Title": "Introduction", 
-	"menu2Title": "Course Content",
+	"menu2Title": "Course Content 2",
+	"menu3Title": "Course Content 3",
 };
 	   
 	    
