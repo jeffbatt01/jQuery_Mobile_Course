a199b578495848f0659b25536c1c37f7743625e2
jeffbatt01 <jeffbatt@gmail.com>
2014-03-25 18:46:32 -0600

    Dynamically build out repeating page elements
    In efforts to try and reduce code I am dynamically adding some of the
    repeating elements on each page. This script happens at the end of the
    index.html file.
    
    Also changed some of the labels within the menuContent.js file.

--- menuContent.js
+++ menuContent.js
@@ -5,8 +5,9 @@
 var JSONObject= 
 {
 	//This is where the page title names will be. I will be updating this to also link to pages. The idea is to dynmaically create the menu based on what is in this JS file.
-	"menu1Title": "Introduction", 
-	"menu2Title": "Course Content",
+	"page1Title": "Introduction", 
+	"page1Link": "page1",
+	"page2Title": "Course Content",
 };
 	   
 	    
