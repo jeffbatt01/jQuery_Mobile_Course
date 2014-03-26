ad9e13d1ac5994827ecfa51afcf11ab5fbf183c8
jeffbatt01 <jeffbatt@gmail.com>
2014-03-25 22:03:05 -0600

    Tied into JSON
    Set up the JSON file to contain the path to the edge animate
    interaction. The idea is to be able to bring in the pages from Edge
    Animate.

--- menuContent.js
+++ menuContent.js
@@ -5,9 +5,27 @@
 var JSONObject= 
 {
 	//This is where the page title names will be. I will be updating this to also link to pages. The idea is to dynmaically create the menu based on what is in this JS file.
+	
+	//Page 1
 	"page1Title": "Introduction", 
 	"page1Link": "page1",
+	"page1Content":"index/Assets/index.html",
+
+	//Page 2
 	"page2Title": "Course Content",
+	"page2Link": "page2",
+	"page2Content": "index/Assets/index.html",
+
+	//Page 3
+	"page3Title": "Page 3 Title",
+	"page3Link": "page3",
+	"page3Content": "boardgame/Assets/index.html",
+
+	//Page 4
+	"page4Title": "Page 4 Title",
+	"page4Link": "page4",
+	"page4Content": "index/Assets/index.html",
+
 };
 	   
 	    
