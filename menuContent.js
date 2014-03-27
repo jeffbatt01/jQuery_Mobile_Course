dea83b52272fe992204d08cb789ae95b847391f7
jeffbatt01 <jeffbatt@gmail.com>
2014-03-26 22:23:59 -0600

    Updated JSON file and paths to JSON

--- menuContent.js
+++ menuContent.js
@@ -1,33 +1,47 @@
-/**
- 
-**/
+/** Course Content **/
 
-var JSONObject= 
-{
+//Menu content
+var menuContent={
 	//This is where the page title names will be. I will be updating this to also link to pages. The idea is to dynmaically create the menu based on what is in this JS file.
 	"menu1Title": "Introduction", 
 	"menu2Title": "Course Content 2",
 	"menu3Title": "Course Content 3",
+
+};
+
+//Page 1
+var page1={
 	
-	//Page 1
-	"page1Title": "Introduction", 
-	"page1Link": "page1",
-	"page1Content":"index/Assets/index.html",
+	"title": "Page 1", 
+	"link": "page1",
+	"path":"index/Assets/index.html",
 
-	//Page 2
-	"page2Title": "Course Content",
-	"page2Link": "page2",
-	"page2Content": "index/Assets/index.html",
+};
 
-	//Page 3
-	"page3Title": "Page 3 Title",
-	"page3Link": "page3",
-	"page3Content": "boardgame/Assets/index.html",
+//Page 2
+var page2={
+	
+	"title": "Page 2", 
+	"link": "page2",
+	"path":"index/Assets/index.html",
 
-	//Page 4
-	"page4Title": "Page 4 Title",
-	"page4Link": "page4",
-	"page4Content": "index/Assets/index.html",
+};
+
+//Page 3
+var page3={
+	
+	"title": "Page 3", 
+	"link": "page3",
+	"path":"boardgame/Assets/index.html",
+
+};
+
+//Page 4
+var page4={
+	
+	"title": "Page 4", 
+	"link": "page4",
+	"path":"index/Assets/index.html",
 
 };
 	   
